1327dd771aeada1918152beb7dd834d3454a2928
mo <mo@2gears.com>
2015-01-27 12:56:03 +0100

    Revert "Implement category blobs Add convex geometry file Change sprite to balls"
    This reverts commit 09625a2690e0fdbf5e4faf6cbb708840748354d5.

--- app/view/three/Graph.js
+++ app/view/three/Graph.js
@@ -15,8 +15,6 @@ Ext.define('Threext.view.three.Graph', {
 	storesToLoad: ['Links', 'Nodes'],
 	initializing: true,
 
-	blobs: {},
-
 	nodes: [],
 	nodeElements: {},
 	nodeAttributes: {
@@ -32,7 +30,7 @@ Ext.define('Threext.view.three.Graph', {
 	nodeUniforms: {
 		texture: {
 			type: "t",
-			value: THREE.ImageUtils.loadTexture("api/textures/sprites/ball.png")
+			value: THREE.ImageUtils.loadTexture("api/textures/sprites/disc.png")
 		}
 	},
 
@@ -100,23 +98,24 @@ Ext.define('Threext.view.three.Graph', {
 
 		window.g = this;
 
-		this.nodeMaterial = new THREE.ShaderMaterial({
-			uniforms: this.nodeUniforms,
-			attributes: this.nodeAttributes,
-			vertexShader: this.nodeVShader,
-			fragmentShader: this.nodeFShader,
-			vertexColors: THREE.VertexColors,
-			transparent: true,
-		});
 
-		this.linkMaterial = new THREE.ShaderMaterial({
-			uniforms: this.linkUniforms,
-			attributes: this.linkAttributes,
-			vertexShader: this.linkVShader,
-			fragmentShader: this.linkFShader,
-			vertexColors: THREE.VertexColors,
-			transparent: true,
-		});
+		this.nodeMaterial = new THREE.ShaderMaterial({
+				uniforms: this.nodeUniforms,
+				attributes: this.nodeAttributes,
+				vertexShader: this.nodeVShader,
+				fragmentShader: this.nodeFShader,
+				vertexColors: THREE.VertexColors,
+				transparent: true,
+			}),
+
+			this.linkMaterial = new THREE.ShaderMaterial({
+				uniforms: this.linkUniforms,
+				attributes: this.linkAttributes,
+				vertexShader: this.linkVShader,
+				fragmentShader: this.linkFShader,
+				vertexColors: THREE.VertexColors,
+				transparent: true,
+			});
 
 		return this.callParent(arguments);
 	},
@@ -137,7 +136,7 @@ Ext.define('Threext.view.three.Graph', {
 		});
 
 		if (this.storesToLoad.length === 0) {
-			if (this.initializing === true) {
+			if(this.initializing === true){
 				this.start();
 			}
 
@@ -151,6 +150,8 @@ Ext.define('Threext.view.three.Graph', {
 		if (this.initializing) {
 			return;
 		}
+
+
 
 		var oldIndex;
 		if (this.nodes) {
@@ -218,12 +219,10 @@ Ext.define('Threext.view.three.Graph', {
 
 	addNodes: function(nodes, scene) {
 		if (this.pointCloud) {
-			scene.remove(this.pointCloud);
-			_.each(this.blobs, function(blob){scene.remove(blob);});
+			this.getScene().remove(this.pointCloud);
 		}
 
-		var material = this.nodeMaterial,
-			blobMaterial = this.blobMaterial;
+		var material = this.nodeMaterial;
 
 		var geometry = new THREE.Geometry(),
 			nodeElements = this.nodeElements;
@@ -231,39 +230,22 @@ Ext.define('Threext.view.three.Graph', {
 		var nodeAttributes = this.nodeAttributes,
 			getColor = this.getColor;
 
-		var catNodes = this.catNodes = {};
-
 		_.each(nodes, function(n, i) {
 			var p = nodeElements[n.id] || new THREE.Vector3(n.x, n.y, n.z);
 			nodeElements[n.id] = p;
 			geometry.vertices.push(p);
 			nodeAttributes.size.value[i] = Number(n.data.size);
 			nodeAttributes.color.value[i] = new THREE.Color(getColor(n.data.category));
-
-			catNodes[n.data.category] = catNodes[n.data.category] || [];
-			catNodes[n.data.category].push(p);
 		});
 
 		var pointCloud = this.pointCloud = new THREE.PointCloud(geometry, material);
-		this.blobs = _.reduce(catNodes, function(blobs, blob, category) {
-			blobs[category] = new THREE.Mesh(new THREE.ConvexGeometry(blob), new THREE.MeshLambertMaterial({
-				color: new THREE.Color(getColor(category)),
-				ambient: new THREE.Color(getColor(category)),
-				opacity: 0.5,
-				transparent: true
-			}));
-			return blobs;
-		}, {});
 		pointCloud.sortParticles = true;
 
 		scene.add(pointCloud);
-		_.each(this.blobs, function(blob) {
-			scene.add(blob);
-		});
 	},
 
 	tick: function() {
-		if (this.initializing) return;
+		if(this.initializing) return;
 
 		var nodes = this.nodes,
 			links = this.links,
@@ -310,12 +292,6 @@ Ext.define('Threext.view.three.Graph', {
 
 		this.pointCloud.geometry.verticesNeedUpdate = true;
 		this.line.geometry.verticesNeedUpdate = true;
-
-		var catNodes = this.catNodes;
-		_.each(this.blobs, function(blob, cat) {
-			blob.geometry = new THREE.ConvexGeometry(catNodes[cat])
-			blob.geometry.verticesNeedUpdate = true;
-		});
 
 		this.nodeAttributes.size.needsUpdate = true;
 		this.nodeAttributes.color.needsUpdate = true;
